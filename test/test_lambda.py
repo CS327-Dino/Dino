664d44e344319e217104fabaeb76ca6ee1771e9e
i-hxrsh <harshvardhan.vala@iitgn.ac.in>
2023-04-10 12:40:08 +0530

    "Added Test Cases for Lambda Function"

--- test/test_lambda.py
+++ test/test_lambda.py
@@ -1,20 +1,88 @@
-from parser.parser import *
-from tokenizing.token_scanning import *
-from datatypes.datatypes import *
-from errors.error import*
 import unittest
+from evaluation.eval import *
+from errors.error import *
+from tokenizing.token_scanning import *
+from parser.parser import *
+from evaluation.eval import *
+from evaluation.resolve import *
+
 class TestParser(unittest.TestCase):
+
     
-    def test_lambda(self):
-        expression = Parser(Scanner('lambda a=12 in a+a end;', DinoError()).generate_tokens(), DinoError()).parse()
-        self.assertEqual(expression,Seq(things=[Lambda(var=Identifier(name='a', line=0, isconst=False, uid=2), e1=Seq(things=[IntLiteral(value=12, line=1)]), e2=Seq(things=[BinOp(left=Identifier(name='a', line=1, isconst=False, uid=0), op=TokenType.PLUS, right=Identifier(name='a', line=1, isconst=False, uid=1), line=1)]), line=0)]))
-
-        expression = Parser(Scanner('lambda a=12 end;', DinoError()).generate_tokens(), DinoError()).parse()
-        self.assertEqual(expression,Seq(things=[Assignment(var=Identifier(name='a', line=0, isconst=False, uid=3), value=Seq(things=[IntLiteral(value=12, line=1)]), line=1, declaration=True)]))
-
+    
+    def test_lambda_0(self):
+        error = DinoError()
+        code = "lambda a=12 in a+a end"
+        scanned_code = Scanner(code, error)
+        token_list = scanned_code.generate_tokens()
+        parser = Parser(token_list, error)
+        expression = parser.parse()
+        resolved = resolution(expression)
+        output = evaluate(resolved)
         
-        expression = Parser(Scanner('lambda v1=true in lambda v2=false in v1 and v2 end end;', DinoError()).generate_tokens(), DinoError()).parse()
-        self.assertEqual(expression,Seq(things=[Lambda(var=Identifier(name='v1', line=0, isconst=False, uid=3), e1=Seq(things=[BoolLiteral(value=True, line=1)]), e2=Seq(things=[Lambda(var=Identifier(name='v2', line=0, isconst=False, uid=2), e1=Seq(things=[BoolLiteral(value=False, line=1)]), e2=Seq(things=[BinOp(left=Identifier(name='v1', line=1, isconst=False, uid=0), op=TokenType.AND, right=Identifier(name='v2', line=1, isconst=False, uid=1), line=1)]), line=0)]), line=0)]))
+        #Should output 12+12=24
+        self.assertEqual(output, 24)
+    def test_lambda_1(self):
+        error = DinoError()
+        code = "lambda a= lambda b = 2 in b+ b*b end in a+a end"
+        scanned_code = Scanner(code, error)
+        token_list = scanned_code.generate_tokens()
+        parser = Parser(token_list, error)
+        expression = parser.parse()
+        resolved = resolution(expression)
+        output = evaluate(resolved)
+        
+        #Should output 12
+        self.assertEqual(output, 12)
+    def test_lambda_2(self):
+        error = DinoError()
+        code = "lambda a= lambda a = 2 in a+ a*a end in a+a end"
+        scanned_code = Scanner(code, error)
+        token_list = scanned_code.generate_tokens()
+        parser = Parser(token_list, error)
+        expression = parser.parse()
+        resolved = resolution(expression)
+        output = evaluate(resolved)
+
+        # Should return 12 as a= 2+2*2 in a+a i.e., a=6 in a+a. 
+
+        self.assertEqual(output, 12)
+
+    def test_lambda_3(self):
+        error = DinoError()
+        code = "lambda a = 12 in lambda b = 7 in b*b+a end end"
+        scanned_code = Scanner(code, error)
+        token_list = scanned_code.generate_tokens()
+        parser = Parser(token_list, error)
+        expression = parser.parse()
+        resolved = resolution(expression)
+        output = evaluate(resolved)
+
+        #7*7+12=61
+        self.assertEqual(output, 61)
+    def test_lambda_4(self):
+        error = DinoError()
+        code = "lambda a = true in lambda b = false in a and b end end"
+        scanned_code = Scanner(code, error)
+        token_list = scanned_code.generate_tokens()
+        parser = Parser(token_list, error)
+        expression = parser.parse()
+        resolved = resolution(expression)
+        output = evaluate(resolved)
+
+        self.assertEqual(output, False)
+    def test_lambda_5(self):
+        error = DinoError()
+        code = "lambda a=2 in a*(lambda b=3 in lambda a=10 in a+b end end) end"
+        scanned_code = Scanner(code, error)
+        token_list = scanned_code.generate_tokens()
+        parser = Parser(token_list, error)
+        expression = parser.parse()
+        resolved = resolution(expression)
+        output = evaluate(resolved)
+        print("Output = ",output)
+        self.assertEqual(output, 26)
+
         
 
     
